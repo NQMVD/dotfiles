a2de986d5433914597becc3cdf461bbab9c4d4c8
ArchNoah <noah.buergler@gmail.com>
2024-05-11 15:13:48 +0200

    added collect

---
+++ collect.zsh
@@ -0,0 +1,9 @@
+
+# collect from paths
+# use gum choose with all selected by default
+# later... use delta to see what files were changed, then use gum choose with all selected by default
+
+# list of configs
+
+
+
